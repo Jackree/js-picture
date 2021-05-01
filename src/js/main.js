3f629b1fee319c71c032e6bf5caf217e93f39d21
Dmitry Ivanov <jackree@yandex.ru>
2021-05-01 15:49:07 +0300

    forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,12 @@
+import forms from "./modules/forms";
 import modals from "./modules/modals";
+import sliders from "./modules/sliders";
 
 window.addEventListener("DOMContentLoaded", () => {
   "use strict";
 
   modals();
+  sliders('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn');
+  sliders('.main-slider-item', 'vertical');
+  forms();
 });
